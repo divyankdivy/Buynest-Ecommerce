9d23f4b1b10c4673ace7d284f28b5f75a189968f
Divyank Rajput <rajputdivyanking@gmail.com>
2025-11-06 22:00:00 +0530

    Add MyEcomAppApplication main class

---
+++ src/main/java/com/myecom/myecomapp/MyEcomAppApplication.java
@@ -0,0 +1,13 @@
+package com.myecom.myecomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEcomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEcomAppApplication.class, args);
+	}
+
+}
